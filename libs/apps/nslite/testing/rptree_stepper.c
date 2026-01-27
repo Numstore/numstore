a2dd1687b9d642be65c7b62b9da858dedcc6578b
Theo Lincke <lincketheo@gmail.com>
2026-01-27 17:58:41 -0500

    smaller inserts

--- libs/apps/nslite/testing/rptree_stepper.c
+++ libs/apps/nslite/testing/rptree_stepper.c
@@ -307,12 +307,11 @@ rptv_stepper_execute (struct rptv_stepper *s, error *e)
       {
         if (current_size > 0)
           {
-            ret = execute_insert (s, rand_range (&s->seed, 0, current_size),
-                                  rand_range (&s->seed, 100, 10000001), e);
+            ret = execute_insert (s, rand_range (&s->seed, 0, current_size), rand_range (&s->seed, 1000, 10000), e);
           }
         else
           {
-            ret = execute_insert (s, 0, rand_range (&s->seed, 100, 10000001), e);
+            ret = execute_insert (s, 0, rand_range (&s->seed, 100, 10000), e);
           }
         break;
       }
@@ -385,7 +384,7 @@ rptv_stepper_execute (struct rptv_stepper *s, error *e)
         if (current_size > 0)
           {
             ret = execute_write (s, rand_range (&s->seed, 0, current_size),
-                                 rand_range (&s->seed, 100, 10000001), 1, e);
+                                 rand_range (&s->seed, 100, 10000), 1, e);
           }
         break;
       }
@@ -467,7 +466,7 @@ rptv_stepper_execute (struct rptv_stepper *s, error *e)
         if (current_size > 0)
           {
             ret = execute_remove (
-                s, rand_range (&s->seed, 0, current_size), rand_range (&s->seed, 10000, 10000001), 1, e);
+                s, rand_range (&s->seed, 0, current_size), rand_range (&s->seed, 10000, 10000), 1, e);
           }
         break;
       }
